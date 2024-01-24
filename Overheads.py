08fcee57b36345013de9b5349ad74977dff95740
Your Name <you@example.com>
2024-01-24 12:49:28 +0800

    overhead import

--- Overheads.py
+++ Overheads.py
@@ -1,19 +1,23 @@
 from pathlib import Path
 import csv
 
-# create a file path to the CSV file
-fp = Path.cwd() / "overheads-day-90.csv"
+# create a file path to csv file in the subfolder named "csv_reports"
+subfolder_name = "csv_reports"
+fp = Path.cwd() / subfolder_name / "overheads-day-90.csv"
+print(fp)
 
-# read the CSV file
+# read the csv file.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
     next(reader)  # skip header
 
-    # create an empty list for cash on hand records as dictionaries
-    overheads = []
+# create an empty list for overheads record
+    overheads=[] 
 
-    # append cash on hand records into the cash_on_hand list as dictionaries
+    # append cash on hand record into the overheads list
     for row in reader:
-        overheads.append({row[0], float(row[1])})
+        #get the "Category", "Overheads" for each record
+        #and append to the overheads list
+        overheads.append([row[0],float(row[1])])   
 
-print(type(overheads[0]))
+print(overheads)
