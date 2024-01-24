dad292454dd81fadebefd4d1e527e751545c3583
oyiling10 <yilingooi13@gmail.com>
2024-01-24 09:51:05 +0800

    Cash on hand import

--- Cash_on_hand.py
+++ Cash_on_hand.py
@@ -1,21 +1,23 @@
 from pathlib import Path
 import csv
-# create a file path to csv file.
-fp = Path.cwd()/"cash-on-hand-sgd.csv"
+
+# create a file path to csv file in the subfolder named "csv_reports"
+subfolder_name = "csv_reports"
+fp = Path.cwd() / subfolder_name / "cash-on-hand-sgd.csv"
 print(fp)
 
 # read the csv file.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
     reader = csv.reader(file)
-    next(reader) # skip header
+    next(reader)  # skip header
 
-    # create an empty list for delivery record
+# create an empty list for cash_on_hand record
     cash_on_hand_sgd=[] 
 
     # append cash on hand record into the cash_on_hand_sgd list
     for row in reader:
-        #get the driver id, sales, distance, and event type for each record
-        #and append to the deliveryRecords list
+        #get the "Day","Cash On Hand" for each record
+        #and append to the cash_on_hand_sgd list
         cash_on_hand_sgd.append([row[0],row[1]])   
 
 print(cash_on_hand_sgd)
