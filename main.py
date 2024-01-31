9d5a4706383753cc42882e048fabeece70cee5d6
tchereplshelp <uptomyowngood@gmail.com>
2024-01-31 14:42:49 +0800

    minor changes

--- main.py
+++ main.py
@@ -9,13 +9,13 @@ with summary_report_path.open(mode="w", encoding="UTF-8") as summary_report:
     summary_report.write("Days with deficit in Cash-on-Hand:\n")
     for day_cash, deficit_amount_cash in analyze_cash_on_hand(cash_on_hand_diff):
         summary_report.write(f"[CASH DEFICIT] DAY {day_cash}, AMOUNT: SGD {deficit_amount_cash}\n")
-    summary_report.write("Top 3 highest deficit amounts in Cash-on-Hand:\n")
+    summary_report.write("Top 3 highest deficit amounts in Cash-on-Hand:\n".upper())
     for i, cash_data in enumerate(analyze_top_deficit_cash(cash_on_hand_diff), start=1):
         summary_report.write(f"[{i}{suffix} HIGHEST CASH DEFICIT] DAY: {cash_data[1]}, AMOUNT: SGD{cash_data[2]}\n")
-    summary_report.write("Days with deficit (profit and loss):\n")
+    summary_report.write("Days with deficit (profit and loss):\n".upper())
     for day_net, deficit_amount_net in analyze_net_profit(net_profit_diff):
         summary_report.write(f"[NET PROFIT DEFICIT] DAY: {day_net}, AMOUNT: SGD{deficit_amount_net}\n")
-    summary_report.write("Top 3 highest deficit amounts in profit and loss:\n")
+    summary_report.write("Top 3 highest deficit amounts in profit and loss:\n".upper())
     for i, nets_data in enumerate(analyze_top_deficit_nets(net_profit_diff), start=1):
         summary_report.write(f"[{i}{suffix} HIGHEST NET PROFIT DEFICIT] DAY: {nets_data[1]}, AMOUNT: SGD{nets_data[2]}\n")
 
